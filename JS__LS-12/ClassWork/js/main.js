037f989413154c9e37f0a611041ccf3564a58326
Omelaa <Ostpa28@gmail.com>
2022-03-15 15:44:32 +0200

    fix cw

--- JS__LS-12/ClassWork/js/main.js
+++ JS__LS-12/ClassWork/js/main.js
@@ -7,50 +7,49 @@
 fetch(`https://jsonplaceholder.typicode.com/posts`)
     .then((response) => response.json())
     .then(posts => {
-        let container = document.createElement('div');
-        container.classList.add('container');
-        let wrap = document.createElement('div');
-        wrap.classList.add('wrapper');
-        for (let post of posts) {
-            let button = document.createElement('button');
-            button.innerText = 'Добавить';
-            button.classList.add('button');
-            let divPost = document.createElement('div');
-            divPost.classList.add('wrapper__item');
-            let postId = document.createElement('span');
-            postId.innerText = `User ID: ${post.userId} Post ID: ${post.id}`;
+        let wrapper = document.createElement('ul');
+        wrapper.classList.add('wrapper');
+        posts.forEach((post) => {
+            let postInner = document.createElement('li');
+            postInner.classList.add('wrapper__item');
+            let id = document.createElement('span');
+            id.innerText = `User ID - ${post.userId}, ID - ${post.id}.`;
             let title = document.createElement('h3');
-            title.innerText = `${post.title}`;
+            title.innerText = `${post.title}.`;
             let body = document.createElement('p');
-            body.innerText = `${post.body}`;
-            divPost.append(postId, title, body, button);
-            wrap.appendChild(divPost);
-            button.addEventListener('click', (event) => {
-                let userCom = {
-                    title: post.title,
-                    body: post.body
-                }
-                let newArr = JSON.parse(localStorage.getItem('posts')) || [];
-                newArr.push(userCom);
-                localStorage.setItem('posts', JSON.stringify(newArr));
-            })
-        }
-        container.appendChild(wrap);
-        document.body.appendChild(container);
-    });
+            body.innerText = `${post.body}.`;
+            let button = document.createElement('button');
+            button.classList.add('button');
+            button.innerText = 'Показать коментарии';
+            postInner.append(id, title, body, button);
+            wrapper.appendChild(postInner);
 
-let comBlock = JSON.parse(localStorage.getItem('posts'));
-let container = document.createElement('div');
-container.classList.add('container');
-let wrap = document.createElement('div');
-wrap.classList.add('wrapper');
-for (let item of comBlock) {
-    let postBlock = document.createElement('div');
-    postBlock.style.padding = '15px';
-    postBlock.style.border = '1px solid #000000';
-    postBlock.style.margin = '0 15px 15px 0';
-    postBlock.innerHTML = `${item.title}<hr>${item.body}`;
-    wrap.appendChild(postBlock);
-}
-container.appendChild(wrap);
-document.body.appendChild(container);
+            button.onclick = () => {
+                fetch('https://jsonplaceholder.typicode.com/posts/' + post.id + '/comments')
+                    .then(response => response.json())
+                    .then(comments => {
+                        comments.forEach((comment) => {
+                            console.log(comment);
+                            let commentWrap = document.createElement('div');
+                            commentWrap.innerHTML = '<b>- Comments -</b>';
+                            commentWrap.style.padding = '15px 0 0 0';
+                            commentWrap.style.borderTop = '1px solid #000000'
+                            commentWrap.style.borderBottom = '1px solid #000000'
+                            commentWrap.style.display = 'grid';
+                            commentWrap.style.gridGap = '15px';
+                            let commentId = document.createElement('h3');
+                            commentId.innerText = `ID - ${comment.id}.`;
+                            let commentName = document.createElement('div');
+                            commentName.innerHTML = `<b>${comment.name}.</b>`;
+                            let commentEmail = document.createElement('div');
+                            commentEmail.innerHTML = `<b>Email - ${comment.email}.</b>   `;
+                            let commentBody = document.createElement('p');
+                            commentBody.innerText = `${comment.body}.`;
+                            commentWrap.append(commentId, commentName, commentEmail, commentBody);
+                            postInner.appendChild(commentWrap);
+                        });
+                    });
+            };
+        });
+        document.body.appendChild(wrapper);
+    });
